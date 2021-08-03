e25ef6cc9e0ca95b795b517240452f984201a2a0
meanii <anilchauhanxda@gmail.com>
2021-08-03 17:42:17 +0530

    added error handler of 404 page

--- walls/views.py
+++ walls/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
 from reddit import getReddit
+
 # Create your views here.
 def result_views(request, *args, **kwargs):
+  search = None
+  error = False 
+  img_list = []
+  
   if request.method == "POST":
     search = dict(request.POST).get('search')
     print(search)
     if isinstance(search, list):
       search = search[0]
-      
-  img_list = getReddit(search, limit=80)
-  return render(request, 'results.html', {'reddit': img_list})
+  
+  try:
+    img_list = getReddit(search, limit=80)
+  except KeyError:
+    error = True
+    
+  content = {
+    'reddit': img_list,
+    'searched': search,
+    'error': error
+  }
+  
+  return render(request, 'results.html', content)
 
 def home_view(request, *args, **kwargs):
   img_list = getReddit('wallpaper', limit=20)
